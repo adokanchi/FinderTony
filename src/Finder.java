1d4c1e8ddaf07c9a07b31cbebcee077b8170b9c2
adokanchi <tony.dokanchi@menloschool.org>
2024-11-01 00:15:06 -0700

    d

--- src/Finder.java
+++ src/Finder.java
@@ -12,46 +12,39 @@ import java.io.IOException;
 
 public class Finder {
     public static final String INVALID = "INVALID KEY";
-    //private static final int p = 249_998_741;
-    private static final int p = 2_000_003;
+    private static final int p = 100_003;
     private static CollisionSet[] map;
 
     public Finder() {}
 
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // n = number
-
         map = new CollisionSet[p+1];
         for (int i = 0; i < p+1; i++) {
             map[i] = new CollisionSet();
         }
+
         String line;
         while ((line = br.readLine()) != null) {
             String[] dataRow = line.split(",");
             String key = dataRow[keyCol];
             String val = dataRow[valCol];
-            Node node = new Node(key, val);
-            int keyHash = hash(key);
-            map[keyHash].add(node);
+            map[hash(key)].add(new Node(key, val));
         }
+
         br.close();
     }
 
     public String query(String key){
-        // Go to the place in the table where all matching hashes are.
-        // (Linear) Search through those hashed values.
         return map[hash(key)].linearSearch(key);
     }
 
     public int hash(String s) {
-        return polyRollingHash(s, 0) % p;
+        return polyRollingHash(s, 0);
     }
 
     public int polyRollingHash(String s, int current) {
         final int R = 256;
-        if (s.isEmpty()) {
-            return current;
-        }
+        if (s.isEmpty()) return current;
         int next = ((current * R) % p + s.charAt(0)) % p;
         return polyRollingHash(s.substring(1), next);
     }
